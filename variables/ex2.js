e420105e860bac401d554cde5aad6c8428de7774
Paul Thurmond <paulthurmond@gmail.com>
2024-08-12 13:12:51 -0500

    Finished

---
+++ variables/ex2.js
@@ -0,0 +1,10 @@
+let age = 58;
+
+let gap = 10;
+console.log(`In ${gap} years, you will be ${age + gap} years old.`);
+gap = 20;
+console.log(`In ${gap} years, you will be ${age + gap} years old.`);
+gap = 30;
+console.log(`In ${gap} years, you will be ${age + gap} years old.`);
+gap = 40;
+console.log(`In ${gap} years, you will be ${age + gap} years old.`);
